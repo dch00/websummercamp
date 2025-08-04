578d20591e012d20a8ad1d837bbb541622ab1532
David Chang <52216372+dch00@users.noreply.github.com>
2025-08-04 21:42:59 +0000

    .

--- src/routes/day1/+page.svelte
+++ src/routes/day1/+page.svelte
@@ -1,24 +1,26 @@
-<div class="prose">
-	<h1>David's Profile</h1>
+<div class="w-full bg-blue-300">
+	<div class="prose p-5">
+		<h1 class="text-center">David's Profile</h1>
 
-	<hr />
+		<hr />
 
-	<div class="h-20 w-20 overflow-hidden rounded-full bg-black">
-		<img
-			src="https://images.unsplash.com/photo-1505628346881-b72b27e84530?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="dog"
-        />
+		<div
+			class="profile-image h-20 w-20 overflow-hidden rounded-full bg-black bg-cover bg-center"
+		></div>
+		<div>David Chang</div>
+		<div>Programmer</div>
+
+		<h2>About Me</h2>
+
+		<p>Hi my name is david.</p>
+
+        <h2>Haiku Exercise</h2>
+
 	</div>
-	<div class="h-20 w-20 overflow-hidden rounded-full bg-black">
-		
-	</div>
-
-	<h2>About Me</h2>
-
-	<p>Hi my name is david.</p>
-
-	<h3>Profile</h3>
-	<h4>Profile</h4>
-	<h5>Profile</h5>
-	<h6>Profile</h6>
 </div>
+
+<style>
+	.profile-image {
+		background-image: url('https://images.unsplash.com/photo-1505628346881-b72b27e84530?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+	}
+</style>

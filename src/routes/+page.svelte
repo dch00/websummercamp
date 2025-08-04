64654e00bbb63e6b66d4f7cd75c6942b43ddf92d
David Chang <52216372+dch00@users.noreply.github.com>
2025-08-04 21:11:41 +0000

    A

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="prose">
+    <div>David Chang</div>
+    <ul>
+        <li><a href="/day1">Day 1</a></li>
+        <li><a href="/day2">Day 2</a></li>
+        <li>Item 3</li>
+    </ul>
+</div>
---
+++ src/routes/day1/+page.svelte
@@ -0,0 +1,24 @@
+<div class="prose">
+	<h1>David's Profile</h1>
+
+	<hr />
+
+	<div class="h-20 w-20 overflow-hidden rounded-full bg-black">
+		<img
+			src="https://images.unsplash.com/photo-1505628346881-b72b27e84530?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="dog"
+        />
+	</div>
+	<div class="h-20 w-20 overflow-hidden rounded-full bg-black">
+		
+	</div>
+
+	<h2>About Me</h2>
+
+	<p>Hi my name is david.</p>
+
+	<h3>Profile</h3>
+	<h4>Profile</h4>
+	<h5>Profile</h5>
+	<h6>Profile</h6>
+</div>

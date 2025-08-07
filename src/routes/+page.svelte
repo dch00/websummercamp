20f321f4ebe2dc5b231cb96be7740eb9cb3738c1
daniel kim <chipperday@gmail.com>
2025-08-07 16:09:04 -0700

    day 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
-<div class="prose">
+<div>
 	<div>David Chang</div>
 	<ul>
 		<li><a href="/day1">Day 1</a></li>
 		<li><a href="/day2">Day 2</a></li>
 		<li><a href="/day3">Day 3</a></li>
+		<li><a href="/day4-1">Day 4 - 1</a></li>
+		<li><a href="/day4-2">Day 4 - 2</a></li>
+		<li><a href="/day4-3">Day 4 - 3</a></li>
 	</ul>
 </div>
---
+++ src/routes/day4-3/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center flex-col">
+	you see a cavern 🌋. what do you do?
+
+	<br />
+	<a href="/day4-3/page2">Yes, go in</a>
+	<br />
+	<a href="/day4-3/page3">No, go to the field</a>
+</div>
